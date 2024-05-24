3d19cb3bbe88924e7a4c47a7e6d63cc8bfd4fd69
jyheo <jyheo@dhitech.co.kr>
2024-05-24 17:02:16 +0900

    [feat]계약서 담당자 추가

--- tongin-front/src/components/detail/firstPage.tsx
+++ tongin-front/src/components/detail/firstPage.tsx
@@ -1388,7 +1388,7 @@ const FirstPage = (props: any) => {
               <FooterItem2>서울시 서초구 양재대로12길 36</FooterItem2>
               <FooterItem3>
                 <div>SERIAL NO.</div>
-                <div>{reNum}</div>
+                <div>{reNum} </div>
               </FooterItem3>
             </FooterItemBox>
             <Index>- 1 -</Index>
